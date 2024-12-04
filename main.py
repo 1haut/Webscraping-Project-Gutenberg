5fd3cb9e20787c6d971add0c7630a39c2858dc06
1haut <ozioma.marshall.anike@funkweb.org>
2024-12-04 09:19:08 +0100

    Standard

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+from bs4 import BeautifulSoup
+import requests
+import time
+import pandas as pd
+
+url = ""
+result = requests.get(url)
+
+html_content = result.text
+
+soup = BeautifulSoup(html_content, "lxml")

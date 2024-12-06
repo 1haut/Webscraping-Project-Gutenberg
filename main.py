3f2644cbcb5274afb5f431194b8957caaad43a72
1haut <ozioma.marshall.anike@funkweb.org>
2024-12-06 09:18:51 +0100

    Basic scraping placeholder url

--- main.py
+++ main.py
@@ -2,10 +2,52 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import pandas as pd
+import json
+import itertools
 
-url = ""
-result = requests.get(url)
+# current_time = time.ctime(time.time())
 
-html_content = result.text
+# cache = {}
 
-soup = BeautifulSoup(html_content, "lxml")
+
+with open("cached_books/cache.json", "r") as books_cache:
+    try:
+        cache = json.load(books_cache)
+    except json.decoder.JSONDecodeError:
+        cache = {}
+        print("The cache is empty.")
+
+
+# Moby Dick Project Gutenberg
+# url = "https://www.gutenberg.org/cache/epub/15/pg15-images.html"
+
+url = "https://books.toscrape.com/catalogue/the-dirty-little-secrets-of-getting-your-dream-job_994/index.html"
+
+
+# Send request if text not in cache
+def requesting_web(url):
+    print("Getting content from web...")
+    result = requests.get(url)
+    return result.text
+
+# Extract text
+def clean_up(html_content):
+    soup = BeautifulSoup(html_content, "lxml")
+    souped_content = soup.find("article", class_ = "product_page")
+    paragraph = souped_content.find("p", recursive=False).text
+    return paragraph
+
+def get_book(url):
+    print("Content loading...")
+    if url in cache:
+        cache[url]
+    else:
+        html_content = requesting_web(url)
+        cache[url] = clean_up(html_content)
+  
+get_book(url)
+
+with open("cached_books/cache.json", "w") as books_cache:
+    books_cache.write(json.dumps(cache))
+
+print(cache[url])
